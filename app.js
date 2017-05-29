885858bd9eb465e5898ef0cd49b87fe7eb63d087
saiharathi <saiharathi94@gmail.com>
2017-05-29 14:59:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	  setBindings();	
+});
+function setBindings(){
+	$("nav a").click(function(e){
+		e.preventDefault();
+		var sectionID = e.currentTarget.id + "Section";
+		
+		
+		 $("html body").animate({
+			 
+			scrollTop:$("#" +sectionID).offset()
+          .top
+			 
+		 },1000)
+		 
+	})
+}
